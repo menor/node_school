d2060b4cd51c691e96a96df4a321132066b066fb
menor <josemenor@gmail.com>
2014-06-06 09:54:19 +0200

    Refactored challenge 10 for added beautyness.

--- 10_time_server.js
+++ 10_time_server.js
@@ -12,28 +12,23 @@
 //    "2013-07-06 07:42"
 
 var net = require( 'net' ),
-    socket = process.argv[2],
+    port = process.argv[2],
     date = new Date();
 
-var server = net.createServer( function( socket ){
-  socket.end( getTime() + '\n' );
-});
-
-function getTime() {
-  var actual_time = '';
-  var month = date.getMonth() + 1;
-  var day = date.getDate();
-  var hour = date.getHours()
-  var minutes = date.getMinutes();
-  actual_time += date.getFullYear() + '-';
-  actual_time +=  month < 10 ? '0'+ month : month;
-  actual_time += '-';
-  actual_time +=  day < 10 ? '0'+ day : day;
-  actual_time += ' ';
-  actual_time += hour < 10 ? '0'+ hour : hour;
-  actual_time += ':';
-  actual_time += minutes < 10 ? '0'+ minutes : minutes;
-  return actual_time;
+function zeroPad( i ){
+  return ( i < 10 ? '0'+ i : i );
 }
 
-server.listen( socket );
+function getCurrentTime() {
+  return date.getFullYear() + '-'
+    + zeroPad( date.getMonth() + 1 ) + '-'
+    + zeroPad( date.getDate() ) + ' '
+    + zeroPad( date.getHours() ) + ':'
+    + zeroPad( date.getMinutes() ); 
+}
+
+var server = net.createServer( function( socket ){
+  socket.end( getCurrentTime() + '\n' );
+});
+
+server.listen( port );
