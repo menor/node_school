fdba13468d888b573bcd9f0333444e5a49acb444
menor <josemenor@gmail.com>
2014-06-06 09:47:13 +0200

    Challenge 10 passes but it's not pretty.

--- 10_time_server.js
+++ 10_time_server.js
@@ -11,3 +11,29 @@
 
 //    "2013-07-06 07:42"
 
+var net = require( 'net' ),
+    socket = process.argv[2],
+    date = new Date();
+
+var server = net.createServer( function( socket ){
+  socket.end( getTime() + '\n' );
+});
+
+function getTime() {
+  var actual_time = '';
+  var month = date.getMonth() + 1;
+  var day = date.getDate();
+  var hour = date.getHours()
+  var minutes = date.getMinutes();
+  actual_time += date.getFullYear() + '-';
+  actual_time +=  month < 10 ? '0'+ month : month;
+  actual_time += '-';
+  actual_time +=  day < 10 ? '0'+ day : day;
+  actual_time += ' ';
+  actual_time += hour < 10 ? '0'+ hour : hour;
+  actual_time += ':';
+  actual_time += minutes < 10 ? '0'+ minutes : minutes;
+  return actual_time;
+}
+
+server.listen( socket );
