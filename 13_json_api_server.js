089c1c8ccfe6487f092327f65ad626d28aa24fee
menor <josemenor@gmail.com>
2014-06-09 14:53:05 +0200

    Change the response handler to a switch statement.

--- 13_json_api_server.js
+++ 13_json_api_server.js
@@ -1,21 +1,25 @@
 var http = require( 'http' );
 var url = require( 'url' );
+var port = process.argv[2];
 
 var server = http.createServer( function( req, res ){
   var requestUrl = url.parse( req.url, true );
   var isoTime = requestUrl.query.iso;
+  console.log( requestUrl );
 
-  if ( requestUrl.pathname === '//api/parsetime' ){
-    console.log( 'API format' );
-  }
-  else if ( requestUrl.pathName === '//api/unixtime' ){
-    console.log( 'Unix' );
-  }
-  else {
-    console.log( 'Invalid API call');
+  switch ( requestUrl.pathname ){
+    case "/api/parsetime":
+      console.log( 'API format' );
+      break;
+    case "/api/unixtime":
+      console.log( 'Unix time' );
+      break;
+    default:
+      console.log( "Invalid API call" );
   }
   res.end();
+  
 });
 
-
+console.log( 'Magic happens at ' + port );
 server.listen( process.argv[2] );
